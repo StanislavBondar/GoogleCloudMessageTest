ac9c5cad713e471f9def6f1036b1a27708f691ea
Aleksandr <aleksandr_mdc@mail.ru>
2016-01-06 22:51:02 +0400

    Google APIs for Mobile: Quickstarts https://github.com/googlesamples/google-services

--- app/src/main/java/com/example/zloj/googlecloudmessagetest/MainActivity.java
+++ app/src/main/java/com/example/zloj/googlecloudmessagetest/MainActivity.java
@@ -1,24 +1,117 @@
 package com.example.zloj.googlecloudmessagetest;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import com.example.zloj.googlecloudmessagetest.gcm_new.QuickstartPreferences;
+import com.example.zloj.googlecloudmessagetest.gcm_new.RegistrationIntentService;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
-    private String Server_API_Key = "AIzaSyDIUDnc_FC--OYR7pFPTswD0vUR_O0j8dw";
-    private String Sender_ID_help = "755649251323";
+
+    private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = "MainActivity";
+
+    private BroadcastReceiver mRegistrationBroadcastReceiver;
+    private ProgressBar mRegistrationProgressBar;
+    private TextView mInformationTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRegistrationProgressBar = (ProgressBar) findViewById(R.id.registrationProgressBar);
+        mRegistrationBroadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                mRegistrationProgressBar.setVisibility(ProgressBar.GONE);
+                SharedPreferences sharedPreferences =
+                        PreferenceManager.getDefaultSharedPreferences(context);
+                boolean sentToken = sharedPreferences
+                        .getBoolean(QuickstartPreferences.SENT_TOKEN_TO_SERVER, false);
+                if (sentToken) {
+                    mInformationTextView.setText(getString(R.string.gcm_send_message));
+                } else {
+                    mInformationTextView.setText(getString(R.string.token_error_message));
+                }
+            }
+        };
+        mInformationTextView = (TextView) findViewById(R.id.informationTextView);
+
+        if (checkPlayServices()) {
+            // Start IntentService to register this application with GCM.
+            Intent intent = new Intent(this, RegistrationIntentService.class);
+            startService(intent);
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        LocalBroadcastManager.getInstance(this).registerReceiver(mRegistrationBroadcastReceiver,
+                new IntentFilter(QuickstartPreferences.REGISTRATION_COMPLETE));
+    }
+
+    @Override
+    protected void onPause() {
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mRegistrationBroadcastReceiver);
+        super.onPause();
+    }
+
+    /**
+     * Check the device to make sure it has the Google Play Services APK. If
+     * it doesn't, display a dialog that allows users to download the APK from
+     * the Google Play Store or enable it in the device's system settings.
+     */
+    private boolean checkPlayServices() {
+        GoogleApiAvailability apiAvailability = GoogleApiAvailability.getInstance();
+        int resultCode = apiAvailability.isGooglePlayServicesAvailable(this);
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (apiAvailability.isUserResolvableError(resultCode)) {
+                apiAvailability.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST)
+                        .show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
 }
 
-//https://developers.google.com/android/guides/setup
-//        https://developers.google.com/mobile/add?platform=android&cntapi=gcm&cnturl=https:%2F%2Fdevelopers.google.com%2Fcloud-messaging%2Fandroid%2Fclient&cntlbl=Continue%20Adding%20GCM%20Support&%3Fconfigured%3Dtrue
-//        https://developers.google.com/cloud-messaging/android/client
-//        https://developers.google.com/cloud-messaging/android/start
-//        https://github.com/google/gcm
-//        https://github.com/googlesamples/google-services/tree/master/android
-//        http://ru.stackoverflow.com/questions/410335/%D0%9A%D0%B0%D0%BA-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE-%D0%BD%D0%B5-%D0%B4%D0%B0%D1%82%D1%8C-%D0%B7%D0%B0%D1%81%D0%BD%D1%83%D1%82%D1%8C-google-cloud-messaging-%D0%BD%D0%B0-android
-//        http://ru.stackoverflow.com/questions/311424/%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2-android-%D0%B2-%D1%81%D0%B2%D0%BE%D0%B5-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5
+
+
+
+//        extends AppCompatActivity {
+//    private String Server_API_Key = "AIzaSyDIUDnc_FC--OYR7pFPTswD0vUR_O0j8dw";
+//    private String Sender_ID_help = "755649251323";
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//    }
+//}
 //
+////https://developers.google.com/android/guides/setup
+////        https://developers.google.com/mobile/add?platform=android&cntapi=gcm&cnturl=https:%2F%2Fdevelopers.google.com%2Fcloud-messaging%2Fandroid%2Fclient&cntlbl=Continue%20Adding%20GCM%20Support&%3Fconfigured%3Dtrue
+////        https://developers.google.com/cloud-messaging/android/client
+////        https://developers.google.com/cloud-messaging/android/start
+////        https://github.com/google/gcm
+////        https://github.com/googlesamples/google-services/tree/master/android
+////        http://ru.stackoverflow.com/questions/410335/%D0%9A%D0%B0%D0%BA-%D0%BF%D1%80%D0%B0%D0%B2%D0%B8%D0%BB%D1%8C%D0%BD%D0%BE-%D0%BD%D0%B5-%D0%B4%D0%B0%D1%82%D1%8C-%D0%B7%D0%B0%D1%81%D0%BD%D1%83%D1%82%D1%8C-google-cloud-messaging-%D0%BD%D0%B0-android
+////        http://ru.stackoverflow.com/questions/311424/%D0%A3%D1%81%D1%82%D0%B0%D0%BD%D0%BE%D0%B2%D0%BA%D0%B0-%D1%83%D0%B2%D0%B5%D0%B4%D0%BE%D0%BC%D0%BB%D0%B5%D0%BD%D0%B8%D0%B9-%D0%B2-android-%D0%B2-%D1%81%D0%B2%D0%BE%D0%B5-%D0%BF%D1%80%D0%B8%D0%BB%D0%BE%D0%B6%D0%B5%D0%BD%D0%B8%D0%B5
+////
---
+++ app/src/main/java/com/example/zloj/googlecloudmessagetest/gcm_new/QuickstartPreferences.java
@@ -0,0 +1,10 @@
+package com.example.zloj.googlecloudmessagetest.gcm_new;
+
+/**
+ * Created by Aleksandr on 06.01.2016.
+ */
+public class QuickstartPreferences {
+    public static final String SENT_TOKEN_TO_SERVER = "sentTokenToServer";
+    public static final String REGISTRATION_COMPLETE = "registrationComplete";
+
+}
